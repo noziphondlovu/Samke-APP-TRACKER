dff40055da1a9d0330ea93f72931016463e383f5
Samkelisiwe <noziphondlovu910@gmail.com>
2022-07-05 13:28:15 +0200

    doing fix problem in js

--- style.js
+++ style.js
@@ -7,20 +7,17 @@ function () {
     }
     else {
         document.querySelector('#tasks').innerHTML
-        += '<div class="task">
-        <span id="taskname">
+        += '<div class= "task">'
+        <span id = "taskname">
         ${document.querySelector('#newtask input').
-        value}
-        </span>
+        value} <span>
         <button class="delete">X</button>
         </div>
         
-
-        var  current_tasks = document.
+        var current_tasks = document.
         querySelector('.delete');
 
-        for (var i = 0; i < current_tasks.length; i+
-        +) {
+        for (var i = 0; i < current_tasks.length; i++) {
             current_tasks[i].onclick = function (){
                 this.parentNode.remove();
             }
