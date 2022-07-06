079c5409f26808bf6e5478064ab4b4ed813cc2e4
Samkelisiwe <noziphondlovu910@gmail.com>
2022-07-06 08:39:59 +0200

    change color white to #1bc1eb

--- style.js
+++ style.js
@@ -6,12 +6,12 @@ var element = document.getElementsByTagName('li');
 // this function will allow us to add elements when we click the button
 btn.onclick = function(){
     
-    var txt = input.value;
-    if(txt ==''){
+    var text = input.value;
+    if(text ==''){
         alert('you must write something');
     }else{
         li = document.createElement('li');
-    li.innerHTML = txt;
+    li.innerHTML = text;
     list.insertBefore(li,list.childNodes[0]);
     input.value = '';
     }
