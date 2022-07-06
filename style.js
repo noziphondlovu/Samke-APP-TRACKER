a4bb2cd72dfb92f73840d9b5094d34b22e2b8cf0
Samkelisiwe <noziphondlovu910@gmail.com>
2022-07-06 10:01:30 +0200

    bfn change between

--- style.js
+++ style.js
@@ -1,9 +1,9 @@
 const input = document.querySelector("#add");
-const  btn = document.querySelector("#btn");
+const  between = document.querySelector("#btn");
 const list = document.querySelector("#list");
 var element = document.getElementsByTagName('li');
 
-// this function will allow us to add elements when we click the button
+
 btn.onclick = function(){
     
     var text = input.value;
@@ -13,14 +13,14 @@ btn.onclick = function(){
         li = document.createElement('li');
     li.innerHTML = text;
     list.insertBefore(li,list.childNodes[0]);
-    input.value = '';
+    input.value = ''
     }
     
-};
+}
 
-//this function will allow us to check the clicked elements
+
 list.onclick = function(ev){
     if(ev.target.tagName == 'LI'){
          ev.target.classList.toggle('checked');
     }
-};
+}
